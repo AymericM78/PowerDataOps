0081e5de28e5296bdd8ff553928d74a5f571bff7
Aymeric <amouille@live.com>
2022-04-14 17:12:49 +0200

    1.0.0.90 Handle smooth transition to oAuth for O365 connectionstrings

--- src/Client/New-XrmClient.ps1
+++ src/Client/New-XrmClient.ps1
@@ -62,18 +62,25 @@ function New-XrmClient {
                 $ConnectionString = Repair-XrbConnectionString -ConnectionString $ConnectionString;
             }
 
-            $XrmClient = Get-CrmConnection -ConnectionString $ConnectionString -MaxCrmConnectionTimeOutMinutes $MaxCrmConnectionTimeOutMinutes -ErrorAction Stop;
-
-            # Warn about Office365 authentication
             $authType = $ConnectionString | Out-XrmConnectionStringParameter -ParameterName "AuthType";
-            if(-not $Quiet -and $authType -eq "Office365")
-            {
-                Write-HostAndLog -Message "============================================================================" -Level WARN;
-                Write-HostAndLog -Message "/!\ Office365 authentication type is deprecated!" -Level WARN;
-                Write-HostAndLog -Message "More info: " -Level WARN;
-                Write-HostAndLog -Message " - https://docs.microsoft.com/fr-fr/power-platform/important-changes-coming#deprecation-of-office365-authentication-type-and-organizationserviceproxy-class-for-connecting-to-dataverse" -Level WARN;
-                Write-HostAndLog -Message "============================================================================" -Level WARN;
+            if($authType -eq "Office365"){
+                
+                # Override O365
+                $ConnectionString = $ConnectionString.Replace("Office365", "OAuth");
+                $ConnectionString = "$ConnectionString;AppId=51f81489-12ee-4a9e-aaae-a2591f45987d; RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;LoginPrompt=Auto;";
+
+                # Warn about Office365 authentication
+                if(-not $Quiet) {
+                    Write-HostAndLog -Message "============================================================================" -Level WARN;
+                    Write-HostAndLog -Message "/!\ Office365 authentication type is deprecated!" -Level WARN;
+                    Write-HostAndLog -Message "Connection string as been modified to force OAuth protocol"  -Level WARN;
+                    Write-HostAndLog -Message "More info: " -Level WARN;
+                    Write-HostAndLog -Message " - https://docs.microsoft.com/fr-fr/power-platform/important-changes-coming#deprecation-of-office365-authentication-type-and-organizationserviceproxy-class-for-connecting-to-dataverse" -Level WARN;
+                    Write-HostAndLog -Message "============================================================================" -Level WARN;
+                }
             }
+
+            $XrmClient = Get-CrmConnection -ConnectionString $ConnectionString -MaxCrmConnectionTimeOutMinutes $MaxCrmConnectionTimeOutMinutes -ErrorAction Stop;            
         }
         else {
             $XrmClient = Get-CrmConnection -InteractiveMode;
